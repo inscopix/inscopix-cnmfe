1dbc39cad534083a0fb6153b36a902fb5b047d1a
brunoboivin <bruno.boivin@mail.mcgill.ca>
2021-02-18 16:31:24 -0800

    DPD-83 library infrastructure

---
+++ include/cnmfe/cnmfe.h
@@ -0,0 +1,11 @@
+#ifndef CNMFE_H
+#define CNMFE_H
+
+using namespace std;
+
+class Cnmfe {
+public:
+    string runCnmfe();
+};
+
+#endif // CNMFE_H
---
+++ src/cnmfe.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "cnmfe/cnmfe.h"
+
+std::string Cnmfe::runCnmfe() {
+    return "CNMFe ran successfully";
+}
