385655eb979cf2f9ce025daa6e950010087e96a6
brunoboivin <bruno.boivin@mail.mcgill.ca>
2021-02-24 10:00:07 -0800

    DPD-86 renamed cnmfe pkg to isx

--- test/testCnmfe.cpp
+++ test/testCnmfe.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "cnmfe/cnmfe.h"
+#include "isx/cnmfe.h"
 
 TEST_CASE("CNMFe completes successfully", "[cnmfe]")
 {
