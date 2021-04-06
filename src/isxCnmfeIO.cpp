293c1686e53447e513f6bd2a300de337c11f3495
brunoboivin <bruno.boivin@mail.mcgill.ca>
2021-04-06 16:07:52 -0700

    DPD-84 write traces to csv

--- src/isxCnmfeIO.cpp
+++ src/isxCnmfeIO.cpp
@@ -1,4 +1,6 @@
 #include "isxCnmfeIO.h"
+#include <fstream>
+#include <iomanip>
 
 namespace isx
 {
@@ -12,5 +14,48 @@ namespace isx
         // data is transposed since armadillo elements are stored in column-major ordering
         footprints.save(arma::hdf5_name(outputFilename, footprintsKey, arma::hdf5_opts::trans + arma::hdf5_opts::append));
         traces.save(arma::hdf5_name(outputFilename, tracesKey, arma::hdf5_opts::trans + arma::hdf5_opts::append));
+    }
+
+    void writeHeaders(
+        std::ofstream & inStream,
+        const std::string & cellNamePrefix,
+        const size_t numCells)
+    {
+        inStream << "Frame";
+        for (size_t cellId(0); cellId < numCells; cellId++)
+        {
+            inStream << "," << cellNamePrefix << std::to_string(cellId);
+        }
+        inStream << std::endl;
+    }
+
+    void writeTraces(
+        std::ofstream & inStream,
+        const MatrixFloat_t & inTraces)
+    {
+        const int32_t maxDecimalsForFloat = std::numeric_limits<float>::digits10 + 1;
+        for (size_t timepoint(0); timepoint < inTraces.n_cols; timepoint++)
+        {
+            inStream << timepoint;
+            for (size_t cellId(0); cellId < inTraces.n_rows; cellId++)
+            {
+                inStream << "," << std::setprecision(maxDecimalsForFloat) << inTraces(cellId, timepoint);
+            }
+            inStream << std::endl;
+        }
+    }
+
+    void saveCnmfeTracesToCSVFile(
+        const MatrixFloat_t & traces,
+        const std::string outputFilename,
+        const std::string cellNamePrefix)
+    {
+        if (traces.n_rows > 0)
+        {
+            std::ofstream outFile(outputFilename);
+            writeHeaders(outFile, cellNamePrefix, traces.n_rows);
+            writeTraces(outFile, traces);
+            outFile.close();
+        }
     }
 }
--- src/isxCnmfeIO.h
+++ src/isxCnmfeIO.h
@@ -5,6 +5,7 @@
 
 namespace isx
 {
+    /// Saves the output of cnmfe to a hierarchical data format file
     ///
     /// \param footprints       Spatial footprints (d1 x d2 x K)
     /// \param traces           Temporal traces (K x T)
@@ -17,6 +18,17 @@ namespace isx
         const std::string outputFilename,
         const std::string footprintsKey = "footprints",
         const std::string tracesKey = "traces");
+
+    /// Saves cnmfe temporal traces to a csv file
+    /// (one row per timepoint, one column per cell)
+    ///
+    /// \param traces           Temporal traces (K x T)
+    /// \param outputFilename   Path to the output file (.csv)
+    /// \param cellNamePrefix   Prefix to use for each cell name (suffix is the cell id)
+    void saveCnmfeTracesToCSVFile(
+        const MatrixFloat_t & traces,
+        const std::string outputFilename,
+        const std::string cellNamePrefix = "C");
 } // namespace isx
 
 #endif //ISX_CNMFE_IO_H
