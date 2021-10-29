e5722406a6620024ad4556b0b46d7f474066ac49
brunoboivin <bruno.boivin@mail.mcgill.ca>
2021-10-29 13:52:43 -0700

    DPD-154 template function for saving cube to tiff

--- test/isxTest.cpp
+++ test/isxTest.cpp
@@ -1,5 +1,4 @@
 #include "isxTest.h"
-#include "isxExportTiff.h"
 #include "catch.hpp"
 
 bool
@@ -13,16 +12,4 @@ approxEqual(
         return inActual == Approx(inExpected).epsilon(inRelTol);
     }
     return inActual == inExpected;
-}
-
-void saveCubeToTiffFile(
-    const isx::CubeFloat_t & inputData,
-    const std::string & outputFilename)
-{
-    std::unique_ptr<isx::TiffExporter> out(new isx::TiffExporter(outputFilename, true));
-    for (size_t i = 0; i < inputData.n_slices; ++i)
-    {
-        out->toTiffOut(inputData.slice(i));
-        out->nextTiffDir();
-    }
 }
--- test/isxTest.h
+++ test/isxTest.h
@@ -2,6 +2,7 @@
 #define ISX_TEST_H
 
 #include "isxArmaUtils.h"
+#include "isxExportTiff.h"
 
 /// Check for approximate equality between two doubles
 ///
@@ -18,13 +19,19 @@ approxEqual(
 
 /// Save an armadillo cube to a tiff stack file
 ///
-/// \param  inActual    The actual value.
-/// \param  inExpected  The expected value.
-/// \param  inRelTol    The relative difference allowed for approximate equality.
-///                     If 0, the values must be exactly equal.
-/// \return             True if the actual value is approximately equal to the expected value.
+/// \param  inputData       Input data in the form of an Armadillo Cube
+/// \param  outputFilename  Path to the output tiff file.
+template <class T>
 void saveCubeToTiffFile(
-    const isx::CubeFloat_t & inputData,
-    const std::string & outputFilename);
+    const arma::Cube<T> & inputData,
+    const std::string & outputFilename)
+{
+    std::unique_ptr<isx::TiffExporter> out(new isx::TiffExporter(outputFilename, true));
+    for (size_t i = 0; i < inputData.n_slices; ++i)
+    {
+        out->toTiffOut(inputData.slice(i));
+        out->nextTiffDir();
+    }
+}
 
 #endif // ISX_TEST_H
