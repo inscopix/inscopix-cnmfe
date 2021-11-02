4e2578c8f18b502cd1325ea3e62d0a4b0279d330
Bruno Boivin <bruno.boivin@mail.mcgill.ca>
2021-11-02 12:06:30 -0700

    DPD-154 testing utilities to generate uint16 and float32 tiff movies (#36)
    * DPD-154 add test utility function to generate tiff movies
    
    * DPD-154 template function for saving cube to tiff
    
    * DPD-154 generate uint16 and float32 tiff files
    
    * DPD-154 remove unneeded iostream lib
    
    * DPD-154 adjust uint16 unit test
    
    * DPD-154 template toTiffOut function to handle uint16 and float32

--- src/io/isxExportTiff.h
+++ src/io/isxExportTiff.h
@@ -2,6 +2,8 @@
 #define ISX_EXPORT_TIFF_H
 
 #include "isxArmaUtils.h"
+#include "isxTiffBuffer.h"
+#include "isxLog.h"
 namespace libtiff {
     // placed in its own namespace to avoid
     // type redefinition conflict with OpenCV
@@ -27,7 +29,64 @@ namespace isx
         /// writes image to current Tiff directory
         ///
         /// \param inImage       pointer to image matrix
-        void toTiffOut(const MatrixFloat_t & inImage);
+        template<typename T>
+        void toTiffOut(const arma::Mat<T> & inImage)
+        {
+            // transpose image to account for armadillo column-major ordering
+            // accordingly references to rows and columns are reversed below
+            const arma::Mat<T> imageTrans = inImage.t();
+            uint32_t width = uint32_t(imageTrans.n_rows);
+            uint32_t height = uint32_t(imageTrans.n_cols);
+
+            uint16_t sampleFormat;
+            if (std::is_same<T, float>::value)
+            {
+                sampleFormat = SAMPLEFORMAT_IEEEFP;
+            }
+            else if (std::is_same<T, uint16_t>::value)
+            {
+                sampleFormat = SAMPLEFORMAT_UINT;
+            }
+            else
+            {
+                ISX_LOG_ERROR("Input datatype not supported. Only F32 and U16 are supported.");
+                throw std::runtime_error("Input datatype not supported. Only F32 and U16 are supported.");
+            }
+
+            uint16_t bitsPerSample = uint16_t(sizeof(T) * 8);
+
+            libtiff::TIFFSetField(out, TIFFTAG_IMAGEWIDTH, width);                    // set the width of the image
+            libtiff::TIFFSetField(out, TIFFTAG_IMAGELENGTH, height);                  // set the height of the image
+            libtiff::TIFFSetField(out, TIFFTAG_SAMPLESPERPIXEL, 1);                   // set number of channels per pixel
+            libtiff::TIFFSetField(out, TIFFTAG_SAMPLEFORMAT, sampleFormat);           // how to interpret each data sample in a pixel
+            libtiff::TIFFSetField(out, TIFFTAG_BITSPERSAMPLE, bitsPerSample);         // set the size of the channels
+            libtiff::TIFFSetField(out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);     // set the origin of the image.
+            libtiff::TIFFSetField(out, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);    // set how the components of each pixel are stored (i.e. RGBRGBRGB or R plane, then G plane, then B plane)
+            libtiff::TIFFSetField(out, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);  // set the color space of the image data
+
+            // The number of bytes between column 0 of two consecutive rows.
+            size_t linebytes = sizeof(T) * imageTrans.n_rows;
+
+            // Allocating memory to store the pixels of current row
+            TIFFBuffer buf(linebytes);
+
+            // We set the strip size of the file to be size of one row of pixels
+            libtiff::TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, 1);
+
+            // The byte array used to store data.
+            const char * pixels = reinterpret_cast<const char *>(imageTrans.memptr());
+
+            for (uint32_t row = 0; row < height; row++)
+            {
+                std::memcpy(buf.get(), &pixels[row*linebytes], linebytes);
+
+                if (libtiff::TIFFWriteScanline(out, buf.get(), row, 0) < 0)
+                {
+                    ISX_LOG_ERROR("TiffExporter: error writing to output file");
+                    throw std::runtime_error("Error writing to output file.");
+                }
+            }
+        }
 
         /// switch to next TIFF Directory/Frame
         ///
--- test/UtilitiesTest.cpp
+++ test/UtilitiesTest.cpp
@@ -1,5 +1,7 @@
 #include "catch.hpp"
 #include "isxUtilities.h"
+#include "isxTest.h"
+#include "isxTiffMovie.h"
 
 #include <QFile>
 
@@ -76,4 +78,78 @@ TEST_CASE("UtilitiesGetBaseName", "[utilities]")
     const std::string expectedBasename = "movie_128x128x100";
     const std::string actualBasename = isx::getBaseName(path);
     REQUIRE(expectedBasename == actualBasename);
+}
+
+TEST_CASE("TestingUtilitiesSaveCubeToTiffFileFloat32", "[testing-utilities]")
+{
+    const float data[60] = {
+        1.03741539f, 21.85586756f, 16.67763465f, 57.27598156f, 38.64771696f,
+        80.373977f, 64.95404632f, 87.34008431f, 61.05377408f, 18.73586471f,
+        64.71167135f, 16.95314142f, 50.18745921f, 41.85081805f, 87.85590855f,
+        89.80071182f, 69.44386778f, 98.33592482f, 86.08319232f, 90.62772769f,
+        49.62817177f, 68.76740932f, 63.80539847f, 79.4547887f, 49.57732931f,
+        24.81011684f, 95.09640316f, 66.68989729f, 82.48704645f, 16.96362576f,
+        97.08460121f, 75.82779868f, 47.79740135f, 70.35291738f, 46.16976625f,
+        16.26060466f, 13.3829529f,  8.40596512f,  3.87483758f, 54.7837783f,
+        46.56684754f, 52.10847115f, 18.55416869f, 34.69019538f, 43.55554648f,
+        92.52000773f,  5.30594245f, 13.87386998f, 14.21110855f, 34.54986401f,
+        69.91073914f, 70.24274241f, 94.05244095f, 94.5517417f, 24.31156753f,
+        46.3534714f, 87.04082964f,  3.94548177f,  3.85709232f, 35.29169532f
+    };
+    const isx::CubeFloat_t inputData(data, 4, 3, 5);
+
+    const std::string outputFilename = "test/data/tmp_movie_float32.tif";;
+    saveCubeToTiffFile(inputData, outputFilename);
+
+    // ensure file exists
+    REQUIRE(isx::pathExists(outputFilename));
+
+    // validate content of the file
+    const isx::SpTiffMovie_t movie = std::shared_ptr<isx::TiffMovie>(new isx::TiffMovie(outputFilename));
+    for (size_t frameIndex = 0; frameIndex < movie->getNumFrames(); frameIndex++)
+    {
+        isx::MatrixFloat_t frame;
+        movie->getFrame(frameIndex, frame);
+        REQUIRE(arma::approx_equal(frame, inputData.slice(frameIndex), "reldiff", 1e-5f));
+    }
+
+    // delete file
+    isx::removeFiles({outputFilename});
+}
+
+TEST_CASE("TestingUtilitiesSaveCubeToTiffFileUint16", "[testing-utilities]")
+{
+    const uint16_t data[60] = {
+        1, 21, 16, 57, 38,
+        80, 64, 87, 61, 18,
+        64, 16, 50, 41, 87,
+        89, 69, 98, 86, 90,
+        49, 68, 63, 79, 49,
+        24, 95, 66, 82, 16,
+        97, 75, 47, 70, 46,
+        16, 13,  8,  3, 54,
+        46, 52, 18, 34, 43,
+        92, 5, 13, 14, 34,
+        69, 70, 94, 94, 24,
+        46, 87,  3,  3, 35
+    };
+    const arma::Cube<uint16_t> inputData(data, 4, 3, 5);
+
+    const std::string outputFilename = "test/data/tmp_movie_uint16.tif";;
+    saveCubeToTiffFile(inputData, outputFilename);
+
+    // ensure file exists
+    REQUIRE(isx::pathExists(outputFilename));
+
+    // validate content of the file
+    const isx::SpTiffMovie_t movie = std::shared_ptr<isx::TiffMovie>(new isx::TiffMovie(outputFilename));
+    for (size_t frameIndex = 0; frameIndex < movie->getNumFrames(); frameIndex++)
+    {
+        arma::Mat<uint16_t> frame;
+        movie->getFrame(frameIndex, frame);
+        REQUIRE(arma::approx_equal(frame, inputData.slice(frameIndex), "abs_tol", 0));
+    }
+
+    // delete file
+    isx::removeFiles({outputFilename});
 }
